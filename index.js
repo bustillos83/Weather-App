f9506d1969999796ced78d77a616fec64c9ebbdf
Fernando Bustillos <105611825+bustillos83@users.noreply.github.com>
2023-10-30 16:59:55 -0400

    update

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const apiKey = "8ae7bbc73b1e8208046843b7d735be9d";
+
+
+const apiKey = "API_KEY";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=imperial&q=New York";
 
 async function checkWeather(){
-    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+    const response = await fetch(apiUrl + `&appid=${API_KEY}`);
     let data = await response.json();
 
     console.log(data);
