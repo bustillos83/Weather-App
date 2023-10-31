5c2712bdf244e8a89be80c332bc5b03eec2fb866
Fernando Bustillos <105611825+bustillos83@users.noreply.github.com>
2023-10-31 12:00:38 -0400

    Added API

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 
+const apiKey = "8ae7bbc73b1e8208046843b7d735be9d"
+;
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=imperial&q=";
 
-const apiKey = "API_KEY";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=imperial&q=New York";
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
 
-async function checkWeather(){
-    const response = await fetch(apiUrl + `&appid=${API_KEY}`);
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city +`&appid=${apiKey}`);
     let data = await response.json();
 
     console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°f";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = Math.round(data.wind.speed) + "mph";
 
 }
 
