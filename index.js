4915009c784eb057e60d87c0537d8e2e53d64cd7
Fernando Bustillos <105611825+bustillos83@users.noreply.github.com>
2023-11-01 09:57:53 -0400

    weather icons changed

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=imperial&
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
 
 async function checkWeather(city){
     const response = await fetch(apiUrl + city +`&appid=${apiKey}`);
@@ -17,8 +18,25 @@ async function checkWeather(city){
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = Math.round(data.wind.speed) + "mph";
 
+    if(data.weather[0].main == "Clouds"){
+        weatherIcon.src = "images/clouds.png";
 
- 
+    }
+    else if(data.weather[0].main == "Clear"){
+        weatherIcon.src = "images/clear.png"
+    }
+    else if(data.weather[0].main == "Rain"){
+        weatherIcon.src = "images/rain.png"
+    }
+    else if(data.weather[0].main == "Drizzle"){
+        weatherIcon.src = "images/drizzle.png"
+    }
+    else if(data.weather[0].main == "Mist"){
+        weatherIcon.src = "images/mist.png"
+    }
+    else if(data.weather[0].main == "Snow"){
+        weatherIcon.src = "images/snow.png"
+    }
     
 }
 
